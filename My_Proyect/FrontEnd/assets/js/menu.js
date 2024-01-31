367bfa880a79eaa4e118edb99e5cc2e1a3f92f72
1031646772 <110569728+1031646772@users.noreply.github.com>
2024-01-31 12:50:38 -0500

    Se añadieron cositas
    nuevos arreglos para el menu

--- My_Proyect/FrontEnd/assets/js/menu.js
+++ My_Proyect/FrontEnd/assets/js/menu.js
@@ -2,18 +2,22 @@
 const boton_menu=document.getElementById("hamburguesa");
 const boton_cerrar=document.getElementById("cerrarMenu");
 const menu=document.getElementById("menu_emer");
-// Se traen las partes del menu
-boton_menu.addEventListener("click", ()=>{
-// se añade evento al boton que cuando le dan click lo muestra
-    menu.style.display="block"
-})
+
+boton_menu.addEventListener("click", () => {
+    // Aplica la animación 'Menu' al abrir el modal
+    menu.style.animation = 'Menu 1s ease-in-out';
+    menu.style.display="flex"
+    body.style.overflow = "hidden";
+});
 
 boton_cerrar.addEventListener("click", () => {
-    //Añadimos eventos para el boton de cerrar, de igual forma cuando le den click
-    //Aqui cambia el estilo del menu añadiendole la animacion de Menu2 para que desaparezca
-    menu.style.animation = 'Menu2 2s ease-in-out';
+    // Agrega la animación 'Menu2' al cerrar el modal
+    menu.style.animation = 'Menu2 1s ease-in-out';
     // Agrega un escuchador para el evento 'animationend'
-    menu.addEventListener("animationend", () => {
-        menu.style.display = "none";
-    }, { once: true }); // La opción { once: true } asegura que el escuchador se elimine después de su primera ejecución
+    menu.addEventListener("animationend", quitacionAnimacion, { once: true });
 });
+
+function quitacionAnimacion(){
+    menu.style.display="none"
+}
+
